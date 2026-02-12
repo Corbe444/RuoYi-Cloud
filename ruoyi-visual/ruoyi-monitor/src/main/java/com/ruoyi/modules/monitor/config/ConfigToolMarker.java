278f13edfab4a4819b53b58e31b48f1d0a5f8a97
Marco Corbetta <73590560+Corbe444@users.noreply.github.com>
2026-02-12 16:40:52 +0100

    add config

---
+++ ruoyi-visual/ruoyi-monitor/src/main/java/com/ruoyi/modules/monitor/config/ConfigToolMarker.java
@@ -0,0 +1,9 @@
+package com.ruoyi.modules.monitor.config;ù
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+@RefreshScope
+@Configuration
+public class ConfigToolMarker {
+}
